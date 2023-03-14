c37e4e7fe3ac6c4b5dd180f8a15cbbecef7d1eb9
mariapopadiuk <mpopadiuk13@gmail.com>
2023-03-14 12:57:06 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 import './App.css';
+import InputFeild from './components/InputFeild';
+import TodoList from './components/TodoList';
+import { Todo } from './model';
 
-function App() {
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>('');
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if(todo){
+      setTodos([...todos, {id: Date.now(), todo, isDone: false}])
+      setTodo('');
+    }
+  };
+  const onDragEnd = (result: DropResult) => {
+const {source, destination} = result;
+
+if(!destination) return;
+if(
+  destination.droppableId === source.droppableId &&
+  destination.index === source.index
+)
+return;
+let add, 
+active = todos,
+complete = completedTodos;
+
+if ( source.droppableId === 'TodoList') {
+  add = active[source.index];
+  active.splice(source.index, 1);
+} else {
+  add = complete[source.index];
+  complete.splice(source.index, 1);
+}
+
+    if (destination.droppableId === 'TodoList') {
+      active.splice(destination.index, 0, add);
+    } else {
+      complete.splice(destination.index, 0, add);
+    }
+
+    setCompletedTodos(complete);
+    setTodos(active);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="App">
+          <span className='heading'>Taskyfy</span>
+          <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+          <TodoList
+            todos={todos}
+            setTodos={setTodos}
+            completedTodos={completedTodos}
+            setCompletedTodos={setCompletedTodos}
+          />
+      </div>
+    </DragDropContext>
+    
   );
 }
 
 export default App;
+
+
+
+// let name: string;
+// let age: number;
+// let isStudent: boolean;
+// let hobbies: string[];
+// let numbers: [number, string];
+// let printName: (name: string) => void;
+// numbers = [1, 'penny'];
+// type Person = {
+//   name: string;
+//   age?: number;
+// }
+// let person: Person;
+// person = {
+//   name: "maria"
+// }
+// function PrintName(name: string){
+//   console.log(name);
+// }
+// PrintName('M');
---
+++ src/model.ts
@@ -0,0 +1,40 @@
+export interface Todo {
+  id: number;
+  todo: string;
+  isDone: boolean;
+}
+
+// type Action = 
+//   | {type: "add"; payload: string}
+//   | { type: "remove"; payload: number }
+//   | { type: "done"; payload: number }
+
+
+// const TodoReducer = (state: Todo[], action: Action) => {
+//  switch (action.type) {
+//   case "add" :
+//     return [
+//       ...state,
+//       {id: Date.now(), todo: action.payload, isDone: false},
+//     ];
+//    case "remove":
+//      return state.filter((todo) => todo.id !== action.payload);
+//    case "done":
+//      return state.map((todo) => 
+//      todo.id !== action.payload ? {...todo, isDone: !todo.isDone}
+//      );
+//      default:
+//       return state;
+//  }
+// };
+
+// import React, { useReducer } from 'react'
+// const ReducerExemple = () =>  {
+//   const [state, dispatch] = useReducer(TodoReducer, []);
+//   return (
+   
+    
+//   )
+// }
+
+// export default ReducerExemple
